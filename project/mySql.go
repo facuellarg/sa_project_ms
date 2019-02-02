ff62f3047bd39f51256d8de0c3636a37dbc83bd0
Freddy <facuellarg@unal.edu.co>
2019-02-02 12:42:29 -0500

    DOckerHub

--- project/mySql.go
+++ project/mySql.go
@@ -10,7 +10,7 @@ func connect() *sql.DB {
 	//local 127.0.0.1:4003
 	//ms 192.168.99.102:4003
 	//cloud 35.237.5.35:4003
-	db, err := sql.Open("mysql", "root:1234@tcp(35.237.5.35:4003)/project")
+	db, err := sql.Open("mysql", "root:1234@tcp(192.168.99.102:4003)/project")
 
 	if err != nil {
 		log.Fatal("Could not connect to database")
