6cd00c168ab2e360c113993ba79c7de58ef7935e
Freddy <facuellarg@unal.edu.co>
2019-02-04 09:04:05 -0500

    IP Nodo1

--- project/mySql.go
+++ project/mySql.go
@@ -9,8 +9,8 @@ import (
 func connect() *sql.DB {
 	//local 127.0.0.1:4003
 	//ms 192.168.99.102:4003
-	//cloud 35.237.5.35:4003
-	db, err := sql.Open("mysql", "root:1234@tcp(35.237.5.35:4003)/project")
+	//cloud 35.227.50.158:4003
+	db, err := sql.Open("mysql", "root:1234@tcp(35.227.50.158:4003)/project")
 
 	if err != nil {
 		log.Fatal("Could not connect to database")
