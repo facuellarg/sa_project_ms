774723387f263525d993de7d20aa5fd65ccebdab
Freddy <facuellarg@unal.edu.co>
2019-02-04 08:58:08 -0500

    Creacion tabla

--- project/main.go
+++ project/main.go
@@ -9,6 +9,8 @@ func main() {
 	router := NewRouter()
 	// local 8000
 	// ms 3003
+
 	log.Println("probando cambios a ver si funcionan ")
+	initDB()
 	log.Fatal(http.ListenAndServe(":3003", router))
 }
--- project/mySql.go
+++ project/mySql.go
@@ -20,3 +20,23 @@ func connect() *sql.DB {
 
 	return db
 }
+
+func initDB() {
+	db := connect()
+	defer db.Close()
+	var statemen string
+	statemen = "CREATE TABLE `projects` (`Project_Id` int(11) NOT NULL AUTO_INCREMENT,`Planning_Id` varchar(400) DEFAULT '',`Estado` varchar(400) DEFAULT '',`Members` varchar(500) DEFAULT '',`ProjectLeader` varchar(100) NOT NULL,`Title` varchar(50) DEFAULT '',`StudyArea` varchar(400) DEFAULT '',`Description` varchar(200) DEFAULT '',PRIMARY KEY (`Project_Id`));"
+	stmt, err := db.Prepare(statemen)
+	if err != nil {
+		log.Fatal("Error al crear la tabla projects")
+	} else {
+		log.Printf("La tabla projects fue creada con exito")
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Fatal("Error al crear la tabla projects")
+	} else {
+		log.Printf("La tabla projects fue creada con exito")
+	}
+
+}
